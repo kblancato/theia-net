98b62751e32c6243c57dd7b1a0304104c739a293
Kirsten Blancato <k.blancato@columbia.edu>
2020-05-11 16:27:34 -0400

    generalize path

--- theia-net/classification/example/data_balanced_classification_evolve.sh
+++ theia-net/classification/example/data_balanced_classification_evolve.sh
@@ -2,11 +2,9 @@
 
 #SBATCH -p cca
 #SBATCH -J job
-#SBATCH -o <path to save output file>/utput_data_balanced_classification_evolve.%j
+#SBATCH -o <path to save output file>/output_data_balanced_classification_evolve.%j
 #SBATCH -e <path to save output file>/error_data_balanced_classification_evolve.%j
 #SBATCH -t 00-00:30:00
-
-DIR="/mnt/home/kblancato/ceph/lc/models/ts/balanced_classification/evolve/run0"
 
 python3 ./run/data.py "0" "<path to code directory>" "<path to home directory>"\
                       "balanced_classification" "evolve" "ts"
