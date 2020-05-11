aadc8f87b7490de99e8faa6202287206305d575c
Kirsten Blancato <k.blancato@columbia.edu>
2020-05-11 16:32:28 -0400

    update paths

--- theia-net/regression/example/select_mcquillan_prot_27.sh
+++ theia-net/regression/example/select_mcquillan_prot_27.sh
@@ -6,7 +6,5 @@
 #SBATCH -e <path to save output file>/error_select_mcquillan_prot_27.%j
 #SBATCH -t 00-01:00:00
 
-DIR="<path to save models>/ts/mcquillan/prot_27/run0"
-
 python3 ./run/select.py 0 "<path to save models>/ts/mcquillan/prot_27/run0" \
                           "prot_27" "27"
